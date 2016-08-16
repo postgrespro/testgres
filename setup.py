af27dd44ee0d2c23e7118c7a175f1288ab89610a
Ildar Musin <zildermann@gmail.com>
2016-08-16 08:59:15 +0300

    prepare package for pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'testgres',
+  packages = ['testgres'],
+  version = '0.1',
+  description = 'Testing utility for postgresql and it''s extensions',
+  author = 'Ildar Musin',
+  author_email = 'zildermann@gmail.com',
+  url = 'https://github.com/postgrespro/testgres',
+  download_url = 'https://github.com/postgrespro/testgres/tarball/0.1',
+  keywords = ['testing', 'postgresql'],
+  classifiers = [],
+)
