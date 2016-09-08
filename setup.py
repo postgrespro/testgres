e5c67f14cefe2b1a600877bd7f9b6503ffb5b37f
Ildar Musin <zildermann@gmail.com>
2016-09-08 19:34:32 +0300

    version 0.1.11

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 setup(
   name = 'testgres',
   packages = ['testgres'],
-  version = '0.1.10',
+  version = '0.1.11',
   description = 'Testing utility for postgresql and it''s extensions',
   author = 'Ildar Musin',
   author_email = 'zildermann@gmail.com',
   url = 'https://github.com/postgrespro/testgres',
-  #download_url = 'https://github.com/postgrespro/testgres/tarball/0.1.1',
   keywords = ['testing', 'postgresql'],
   classifiers = [],
   install_requires = ["pg8000", "six"]
