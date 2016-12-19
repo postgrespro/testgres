ef1da6cc447fbecb511b63d2d8c3c090b02c9868
Dmitry Ivanov <ivadmi5@gmail.com>
2016-12-19 15:13:49 +0300

    update version (0.1.18)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='testgres',
     packages=['testgres'],
-    version='0.1.17',
+    version='0.1.18',
     description='Testing utility for postgresql and its extensions',
     author='Ildar Musin',
     author_email='zildermann@gmail.com',
