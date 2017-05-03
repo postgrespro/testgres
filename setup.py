46e77926b58b8c527117c4f63244ed65d80cac80
Dmitry Ivanov <ivadmi5@gmail.com>
2017-05-03 18:10:30 +0300

    bump version (0.2.0)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='testgres',
     packages=['testgres'],
-    version='0.1.19',
+    version='0.2.0',
     description='Testing utility for postgresql and its extensions',
     author='Ildar Musin',
     author_email='zildermann@gmail.com',
