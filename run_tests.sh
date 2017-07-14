e03322e8024b6e0d3804bc01ecbeb987d50232c9
Dmitry Ivanov <ivadmi5@gmail.com>
2017-07-14 18:38:45 +0300

    improve tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+set -eux
+
 # DEBUG port-for
 cat /proc/sys/net/ipv4/ip_local_port_range
 
 cd testgres/tests
 ${PYTHON} -m unittest test_simple
+
+cd ../..
 flake8 --ignore=W191,F401,E501,F403 .
