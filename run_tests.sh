44321bfff5ab7c741e36851d441cafc7b60b3b2b
Dmitry Ivanov <ivadmi5@gmail.com>
2017-09-13 15:58:32 +0300

    check coverage using Codecov.io

--- run_tests.sh
+++ run_tests.sh
@@ -2,10 +2,33 @@
 
 set -eux
 
-cd testgres/tests
-${PYTHON} -m unittest test_simple
+if [ "$PYTHON_VERSION" -eq "2" ]; then
+	virtualenv="virtualenv --python=/usr/bin/python2"
+	pip=pip2
+else
+	virtualenv="virtualenv --python=/usr/bin/python3"
+	pip=pip3
+fi
 
-cd ../..
+# prepare environment
+$virtualenv env
+source env/bin/activate
+
+# install utilities
+$pip install coverage codecov flake8
+
+# install testgres
+$pip install -U .
+
+# test code quality
 flake8 .
 
-cd ../..
+# run tests
+cd testgres/tests
+coverage run test_simple.py
+
+# show coverage
+coverage report
+
+# gather reports
+codecov
