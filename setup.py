6e64203c39331fd1da4b9c2c45a71eed9497ae72
Dmitry Ivanov <ivadmi5@gmail.com>
2017-11-01 19:15:08 +0300

    bump version to 1.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='testgres',
     packages=['testgres'],
-    version='1.3.1',
+    version='1.3.2',
     description='Testing utility for PostgreSQL and its extensions',
     license='PostgreSQL',
     author='Ildar Musin',
