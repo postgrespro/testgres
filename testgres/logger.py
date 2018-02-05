24221e673fc0bab0c6d34bc24d511d9e49007e28
Dmitry Ivanov <ivadmi5@gmail.com>
2018-02-05 12:28:00 +0300

    small fix for logger

--- testgres/logger.py
+++ testgres/logger.py
@@ -25,15 +25,20 @@ class TestgresLogger(threading.Thread):
         with open(self._log_file_name, 'r') as fd:
             # work until we're asked to stop
             while not self._stop_event.is_set():
+                sleep_time = 0.1
+                new_lines = False
+
                 # do we have new lines?
                 if fd in select.select([fd], [], [], 0)[0]:
                     for line in fd.readlines():
                         line = line.strip()
                         if line:
+                            new_lines = True
                             extra = {'node': self._node_name}
                             self._logger.info(line, extra=extra)
-                else:
-                    time.sleep(0.1)
+
+                if not new_lines:
+                    time.sleep(sleep_time)
 
             # don't forget to clear event
             self._stop_event.clear()
