8c199a40c67deb88a2cf46e245f12fd734e3852b
Ildar Musin <zildermann@gmail.com>
2018-03-21 14:27:05 +0300

    Remove redundant comments from Sphinx configuration script

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,10 +38,6 @@ release = u'1.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = [
-#    'sphinx.ext.autodoc',
-#]
-
 extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
