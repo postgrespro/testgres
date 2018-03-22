749de80e9de4f61dbc4ee6825cb36e64287bbb4d
Ildar Musin <zildermann@gmail.com>
2018-03-22 14:05:24 +0300

    Use __class__.__name__ instead of 'PostgresNode' literal

--- testgres/node.py
+++ testgres/node.py
@@ -136,8 +136,8 @@ class PostgresNode(object):
             self._try_shutdown(attempts)
 
     def __repr__(self):
-        return "PostgresNode(name='{}', port={}, base_dir='{}')".format(
-            self.name, self.port, self.base_dir)
+        return "{}(name='{}', port={}, base_dir='{}')".format(
+            self.__class__.__name__, self.name, self.port, self.base_dir)
 
     @property
     def pid(self):
