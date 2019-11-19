87a0491f3b1ab5b2bf59f3c5d76b01985b8daba5
Arseny Sher <sher-ars@yandex.ru>
2019-11-19 15:17:13 +0300

    Try to fix caughtup in LR tests.

--- testgres/pubsub.py
+++ testgres/pubsub.py
@@ -186,12 +186,19 @@ class Subscription(object):
         Args:
             username: remote node's user name.
         """
-        query = """
-            select pg_current_wal_lsn() - replay_lsn = 0
-            from pg_catalog.pg_stat_replication where application_name = '{}'
-        """.format(self.name)
 
         try:
+            pub_lsn = pub.node.execute(query="select pg_current_wal_lsn()::text from pg_catalog.pg_stat_replication",
+                                       dbname=None,
+                                       username=None)  # yapf: disable
+            # create dummy xact
+            pub.node.execute(query="select txid_current()", dbname=None, username=None)
+            query = """
+            select {} - replay_lsn <= 0
+            from pg_catalog.pg_stat_replication where application_name = '{}'
+            """.format(pub_lsn, self.name)
+
+
             # wait until this LSN reaches subscriber
             self.pub.node.poll_query_until(
                 query=query,
