cf91a745f5920255c9f4acb4a930b0900d189e65
vshepard <v.shepard@postgrespro.ru>
2024-01-30 07:42:19 +0100

    Move file control for tests

--- testgres/plugins/__init__.py
+++ testgres/plugins/__init__.py
@@ -2,8 +2,7 @@ from pg_probackup2.gdb import GDBobj
 from pg_probackup2.app import ProbackupApp, ProbackupException
 from pg_probackup2.init_helpers import init_params
 from pg_probackup2.storage.fs_backup import FSTestBackupDir
-from pg_probackup2.storage.s3_backup import S3TestBackupDir
 
 __all__ = [
-    "ProbackupApp", "ProbackupException", "init_params", "FSTestBackupDir", "S3TestBackupDir", "GDBobj"
+    "ProbackupApp", "ProbackupException", "init_params", "FSTestBackupDir", "GDBobj"
 ]
