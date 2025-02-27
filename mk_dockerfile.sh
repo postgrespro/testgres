bcb77713fb0554a68258b213cf3c91bf775a5401
d.kovalenko <dmitry.lipetsk@gmail.com>
2025-02-27 17:38:36 +0300

    CI-test on Ubuntu 24.04 is added.

--- mk_dockerfile.sh
+++ mk_dockerfile.sh
@@ -1,2 +1,2 @@
 set -eu
-sed -e 's/${PYTHON_VERSION}/'${PYTHON_VERSION}/g -e 's/${PG_VERSION}/'${PG_VERSION}/g Dockerfile.tmpl > Dockerfile
+sed -e 's/${PYTHON_VERSION}/'${PYTHON_VERSION}/g -e 's/${PG_VERSION}/'${PG_VERSION}/g Dockerfile--${TEST_PLATFORM}.tmpl > Dockerfile
