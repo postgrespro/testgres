e47cded2a784bfbc64c9a70aceedfe26f9a3b802
d.kovalenko <dmitry.lipetsk@gmail.com>
2025-03-11 10:27:59 +0300

    [remote_ops] A problem with mktemp on Alpine Linux is fixed
    Five 'X' in template is not enough - Alpine returns "mktemp: : Invalid argument" error.
    
    Six 'X' is OK.

--- tests/test_local.py
+++ tests/test_local.py
@@ -4,6 +4,7 @@ import os
 import pytest
 import re
 import tempfile
+import logging
 
 from ..testgres import ExecUtilException
 from ..testgres import InvalidOperationException
@@ -17,6 +18,22 @@ class TestLocalOperations:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self):
         self.operations = LocalOperations()
+
+    def test_mkdtemp__default(self):
+        path = self.operations.mkdtemp()
+        logging.info("Path is [{0}].".format(path))
+        assert os.path.exists(path)
+        os.rmdir(path)
+        assert not os.path.exists(path)
+
+    def test_mkdtemp__custom(self):
+        C_TEMPLATE = "abcdef"
+        path = self.operations.mkdtemp(C_TEMPLATE)
+        logging.info("Path is [{0}].".format(path))
+        assert os.path.exists(path)
+        assert C_TEMPLATE in os.path.basename(path)
+        os.rmdir(path)
+        assert not os.path.exists(path)
 
     def test_exec_command_success(self):
         """
