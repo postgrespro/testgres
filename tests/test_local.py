e5be783f430880286f408927bd9aacbd30651069
d.kovalenko <dmitry.lipetsk@gmail.com>
2025-04-02 17:31:50 +0300

    TestLocalOperations is refactored
    - [del] setup
    - [add] fixture: os_ops

--- tests/test_local.py
+++ tests/test_local.py
@@ -1,27 +1,27 @@
 # coding: utf-8
+from .helpers.os_ops_descrs import OsOpsDescrs
+from .helpers.os_ops_descrs import OsOperations
+
 import os
 
 import pytest
 import re
 
-from ..testgres import LocalOperations
-
 
 class TestLocalOperations:
+    @pytest.fixture
+    def os_ops(self):
+        return OsOpsDescrs.sm_local_os_ops
 
-    @pytest.fixture(scope="function", autouse=True)
-    def setup(self):
-        self.operations = LocalOperations()
-
-    def test_read__unknown_file(self):
+    def test_read__unknown_file(self, os_ops: OsOperations):
         """
         Test LocalOperations::read with unknown file.
         """
 
         with pytest.raises(FileNotFoundError, match=re.escape("[Errno 2] No such file or directory: '/dummy'")):
-            self.operations.read("/dummy")
+            os_ops.read("/dummy")
 
-    def test_read_binary__spec__unk_file(self):
+    def test_read_binary__spec__unk_file(self, os_ops: OsOperations):
         """
         Test LocalOperations::read_binary with unknown file.
         """
@@ -29,21 +29,24 @@ class TestLocalOperations:
         with pytest.raises(
                 FileNotFoundError,
                 match=re.escape("[Errno 2] No such file or directory: '/dummy'")):
-            self.operations.read_binary("/dummy", 0)
+            os_ops.read_binary("/dummy", 0)
 
-    def test_get_file_size__unk_file(self):
+    def test_get_file_size__unk_file(self, os_ops: OsOperations):
         """
         Test LocalOperations::get_file_size.
         """
+        assert isinstance(os_ops, OsOperations)
 
         with pytest.raises(FileNotFoundError, match=re.escape("[Errno 2] No such file or directory: '/dummy'")):
-            self.operations.get_file_size("/dummy")
+            os_ops.get_file_size("/dummy")
 
-    def test_cwd(self):
+    def test_cwd(self, os_ops: OsOperations):
         """
         Test cwd.
         """
-        v = self.operations.cwd()
+        assert isinstance(os_ops, OsOperations)
+
+        v = os_ops.cwd()
 
         assert v is not None
         assert type(v) == str  # noqa: E721
