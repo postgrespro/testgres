696cc1ef8a4ba6e438057f773a9833e0fea0eb08
d.kovalenko <dmitry.lipetsk@gmail.com>
2025-04-06 10:55:24 +0300

    PortManager__Generic is refactored

--- testgres/port_manager.py
+++ testgres/port_manager.py
@@ -48,45 +48,55 @@ class PortManager__ThisHost(PortManager):
 
 class PortManager__Generic(PortManager):
     _os_ops: OsOperations
-    _allocated_ports_guard: object
-    _allocated_ports: set[int]
+    _guard: object
+    # TODO: is there better to use bitmap fot _available_ports?
+    _available_ports: set[int]
+    _reserved_ports: set[int]
 
     def __init__(self, os_ops: OsOperations):
         assert os_ops is not None
         assert isinstance(os_ops, OsOperations)
         self._os_ops = os_ops
-        self._allocated_ports_guard = threading.Lock()
-        self._allocated_ports = set[int]()
+        self._guard = threading.Lock()
+        self._available_ports = set[int](range(1024, 65535))
+        self._reserved_ports = set[int]()
 
     def reserve_port(self) -> int:
-        ports = set(range(1024, 65535))
-        assert type(ports) == set  # noqa: E721
+        assert self._guard is not None
+        assert type(self._available_ports) == set  # noqa: E721t
+        assert type(self._reserved_ports) == set  # noqa: E721
 
-        assert self._allocated_ports_guard is not None
-        assert type(self._allocated_ports) == set  # noqa: E721
-
-        with self._allocated_ports_guard:
-            ports.difference_update(self._allocated_ports)
-
-            sampled_ports = random.sample(tuple(ports), min(len(ports), 100))
+        with self._guard:
+            t = tuple(self._available_ports)
+            assert len(t) == len(self._available_ports)
+            sampled_ports = random.sample(t, min(len(t), 100))
+            t = None
 
             for port in sampled_ports:
-                assert not (port in self._allocated_ports)
+                assert not (port in self._reserved_ports)
+                assert port in self._available_ports
 
                 if not self._os_ops.is_port_free(port):
                     continue
 
-                self._allocated_ports.add(port)
+                self._reserved_ports.add(port)
+                self._available_ports.discard(port)
+                assert port in self._reserved_ports
+                assert not (port in self._available_ports)
                 return port
 
-        raise PortForException("Can't select a port")
+        raise PortForException("Can't select a port.")
 
     def release_port(self, number: int) -> None:
         assert type(number) == int  # noqa: E721
 
-        assert self._allocated_ports_guard is not None
-        assert type(self._allocated_ports) == set  # noqa: E721
+        assert self._guard is not None
+        assert type(self._reserved_ports) == set  # noqa: E721
 
-        with self._allocated_ports_guard:
-            assert number in self._allocated_ports
-            self._allocated_ports.discard(number)
+        with self._guard:
+            assert number in self._reserved_ports
+            assert not (number in self._available_ports)
+            self._available_ports.add(number)
+            self._reserved_ports.discard(number)
+            assert not (number in self._reserved_ports)
+            assert number in self._available_ports
