9515b023f2a581d6f9eba638d10ce9c9cfc6b6ff
Dmitry Kovalenko <dmitry.lipetsk@gmail.com>
2026-01-04 17:50:55 +0300

    PostgresNode::child_processes works on a running node only (#303)
    * PostgresNode::child_processes works on a running node only
    
    We will raise an exception InvalidOperationException if PostgresNode::child_processes is called on a stopped or uninitialized node.

---
+++ tests/test_raise_error.py
@@ -0,0 +1,65 @@
+from src import InvalidOperationException
+from src import NodeStatus
+from src.raise_error import RaiseError
+
+import pytest
+import typing
+
+
+class TestRaiseError:
+    class tagData001__NodeErr_CantEnumerateChildProcesses:
+        node_status: NodeStatus
+        expected_msg: str
+
+        def __init__(
+            self,
+            node_status: NodeStatus,
+            expected_msg: str,
+        ):
+            assert type(node_status) == NodeStatus  # noqa: E721
+            assert type(expected_msg) == str  # noqa: E721
+            self.node_status = node_status
+            self.expected_msg = expected_msg
+            return
+
+        @property
+        def sign(self) -> str:
+            assert type(self.node_status) == NodeStatus  # noqa: E721
+
+            msg = "status: {}".format(self.node_status)
+            return msg
+
+    sm_Data001: typing.List[tagData001__NodeErr_CantEnumerateChildProcesses] = [
+        tagData001__NodeErr_CantEnumerateChildProcesses(
+            NodeStatus.Uninitialized,
+            "Can't enumerate node child processes. Node is not initialized.",
+        ),
+        tagData001__NodeErr_CantEnumerateChildProcesses(
+            NodeStatus.Stopped,
+            "Can't enumerate node child processes. Node is not running.",
+        ),
+    ]
+
+    @pytest.fixture(
+        params=sm_Data001,
+        ids=[x.sign for x in sm_Data001],
+    )
+    def data001(self, request: pytest.FixtureRequest) -> tagData001__NodeErr_CantEnumerateChildProcesses:
+        assert isinstance(request, pytest.FixtureRequest)
+        assert type(request.param).__name__ == "tagData001__NodeErr_CantEnumerateChildProcesses"
+        return request.param
+
+    def test_001__node_err__cant_enumerate_child_processes(
+        self,
+        data001: tagData001__NodeErr_CantEnumerateChildProcesses,
+    ):
+        assert type(data001) == __class__.tagData001__NodeErr_CantEnumerateChildProcesses  # noqa: E721
+
+        with pytest.raises(expected_exception=InvalidOperationException) as x:
+            RaiseError.node_err__cant_enumerate_child_processes(
+                data001.node_status
+            )
+
+        assert x is not None
+        assert str(x.value) == data001.expected_msg
+        return
