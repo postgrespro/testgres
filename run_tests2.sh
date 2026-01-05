ba79a31f5065c78c0f47d77cf7a4105a603d2407
Dmitry Kovalenko <dmitry.lipetsk@gmail.com>
2026-01-06 00:08:44 +0300

    pyproject.toml is used (#307)
    setup.cfg, setup.py, MANIFEST.in were removed.

--- run_tests2.sh
+++ run_tests2.sh
@@ -25,42 +25,46 @@ python -m venv "${VENV_PATH}"
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 source "${VENV_PATH}/bin/activate"
 pip install -r tests/requirements.txt
-pip install coverage flake8 Sphinx
 
 # install testgres' dependencies
 export PYTHONPATH=$(pwd)
 # $PIP install .
 
 # test code quality
+pip install flake8 flake8-pyproject
 flake8 .
-
+pip uninstall -y flake8 flake8-pyproject
 
 # remove existing coverage file
 export COVERAGE_FILE=.coverage
 rm -f $COVERAGE_FILE
 
+pip install coverage
 
 # run tests (PATH)
 time coverage run -a -m pytest -l -v -n 4 -k "${TEST_FILTER}"
-
 
 # run tests (PG_BIN)
 PG_BIN=$(pg_config --bindir) \
 time coverage run -a -m pytest -l -v -n 4 -k "${TEST_FILTER}"
 
-
 # run tests (PG_CONFIG)
 PG_CONFIG=$(pg_config --bindir)/pg_config \
 time coverage run -a -m pytest -l -v -n 4 -k "${TEST_FILTER}"
 
-
 # show coverage
 coverage report
 
+pip uninstall -y coverage
+
 # build documentation
+pip install Sphinx
+
 cd docs
 make html
 cd ..
+
+pip uninstall -y Sphinx
 
 # attempt to fix codecov
 set +eux
