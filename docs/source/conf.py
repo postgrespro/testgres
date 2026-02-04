9b011a295e99f3f2f13f6822e518166a2a743f12
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-02-04 22:07:40 +0300

    Version 1.13.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ author = u'Postgres Professional'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1.13.4'
+release = u'1.13.5'
 
 # -- General configuration ---------------------------------------------------
 
