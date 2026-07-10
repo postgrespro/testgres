b171874bcd8cb3b1f904a84f0c560e3716da5082
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-07-10 18:49:06 +0300

    fix: TestgresLogger did not use os_ops

---
+++ src/impl/file_line_reader.py
@@ -0,0 +1,80 @@
+# coding: utf-8
+
+import typing
+
+from testgres.operations.os_ops import OsOperations
+
+
+class FileLineReader:
+    _os_ops: OsOperations
+    _file_name: str
+    _file_encoding: str
+    _file_pos: int
+    _buffer_pos: int
+    _buffer: bytes
+
+    # --------------------------------------------------------------------
+    def __init__(
+        self,
+        os_ops: OsOperations,
+        file_name: str,
+        file_encoding: str = "utf-8",
+    ):
+        assert isinstance(os_ops, OsOperations)
+        assert type(file_encoding) is str
+        self._os_ops = os_ops
+        self._file_name = file_name
+        self._file_encoding = file_encoding
+        self._file_pos = 0
+        self._buffer_pos = 0
+        self._buffer = b''
+        return
+
+    # interface ----------------------------------------------------------
+    def read_line(self) -> typing.Optional[str]:
+        assert isinstance(self._os_ops, OsOperations)
+        assert type(self._buffer_pos) is int
+        assert type(self._buffer) is bytes
+        assert self._buffer_pos >= 0
+        assert self._buffer_pos <= len(self._buffer)
+
+        scan_pos = self._buffer_pos
+
+        while True:
+            sz1 = len(self._buffer)
+
+            if scan_pos == sz1:
+                block = self._os_ops.read_binary(
+                    self._file_name,
+                    self._file_pos,
+                )
+                assert type(block) is bytes
+                self._buffer += block
+                self._file_pos += len(block)
+
+            x = self._buffer.find(b'\n', scan_pos)
+
+            sz2 = len(self._buffer)
+
+            if x == -1:
+                if scan_pos == sz2:
+                    return None
+
+                if self._buffer_pos == 0:
+                    scan_pos = sz2
+                else:
+                    assert self._buffer_pos > 0
+                    self._buffer = self._buffer[self._buffer_pos:]
+                    scan_pos = sz2 - self._buffer_pos
+                    self._buffer_pos = 0
+                continue
+
+            assert x >= 0
+            assert x < sz2
+
+            b = self._buffer[self._buffer_pos:(x+1)]
+
+            s = b.decode(self._file_encoding)
+
+            self._buffer_pos = x + 1
+            return s
--- src/logger.py
+++ src/logger.py
@@ -1,49 +1,116 @@
 # coding: utf-8
 
+from .config import testgres_config as tconf
+from .exceptions import ExecUtilException
+from .impl.file_line_reader import FileLineReader
+
+from testgres.operations.os_ops import OsOperations
+
 import logging
-import select
 import threading
 import time
+import typing
 
 
 class TestgresLogger(threading.Thread):
+    _C_SLEEP_MIN = 0.01
+    _C_SLEEP_MAX = 60
+
     """
     Helper class to implement reading from log files.
     """
-    def __init__(self, node_name, log_file_name):
+    def __init__(
+        self,
+        node_name: str,
+        log_file_name: str,
+        log_file_encoding: str = "utf-8",
+        os_ops: typing.Optional[OsOperations] = None,
+    ):
+        assert type(node_name) is str
+        assert type(log_file_name) is str
+        assert type(log_file_encoding) is str
+        assert os_ops is None or isinstance(os_ops, OsOperations)
+
         threading.Thread.__init__(self)
 
+        if os_ops is None:
+            os_ops = tconf.os_ops
+
+        assert os_ops is None or isinstance(os_ops, OsOperations)
+
+        self._os_ops = os_ops
         self._node_name = node_name
         self._log_file_name = log_file_name
+        self._log_file_encoding = log_file_encoding
         self._stop_event = threading.Event()
         self._logger = logging.getLogger(node_name)
         self._logger.setLevel(logging.INFO)
+        return
 
     def run(self):
         # open log file for reading
-        with open(self._log_file_name, 'r') as fd:
+        file_line_reader = FileLineReader(
+            self._os_ops,
+            self._log_file_name,
+            self._log_file_encoding,
+        )
+
+        sleep_time = __class__._C_SLEEP_MIN
+
+        try:
             # work until we're asked to stop
             while not self._stop_event.is_set():
-                sleep_time = 0.1
-                new_lines = False
+                line = None
+
+                while True:
+                    try:
+                        line = file_line_reader.read_line()  # raise
+                    except Exception as e:
+                        if __class__._is_file_not_found_exception(e):
+                            if not self._os_ops.path_exists(self._log_file_name):
+                                break
+                        raise
+                    break
+
+                if line is None:
+                    time.sleep(sleep_time)
+                    sleep_time = min(__class__._C_SLEEP_MAX, 2 * sleep_time)
+                    continue
+
+                assert type(line) is str
+
+                sleep_time = __class__._C_SLEEP_MIN
 
                 # do we have new lines?
-                if fd in select.select([fd], [], [], 0)[0]:
-                    for line in fd.readlines():
-                        line = line.strip()
-                        if line:
-                            new_lines = True
-                            extra = {'node': self._node_name}
-                            self._logger.info(line, extra=extra)
+                line = line.strip()
 
-                if not new_lines:
-                    time.sleep(sleep_time)
-
+                extra = {'node': self._node_name}
+                self._logger.info(line, extra=extra)
+                continue
+        except Exception as e:
+            self._logger.error(e)
+            raise
+        finally:
             # don't forget to clear event
-            self._stop_event.clear()
+            # [2026-07-10] legacy cargo cult, thread is single-use only.
+            # self._stop_event.clear()
+            pass
+        return
 
     def stop(self, wait=True):
         self._stop_event.set()
 
         if wait:
             self.join()
+        return
+
+    @staticmethod
+    def _is_file_not_found_exception(e: Exception) -> bool:
+        if isinstance(e, FileNotFoundError):
+            return True
+
+        if isinstance(e, ExecUtilException):
+            if e.exit_code == 2:
+                return True
+
+        return False
