6da409c978899b48d12e7550696bd60fb09b9474
Dmitry Kovalenko <dmitry.lipetsk@gmail.com>
2026-07-14 15:38:54 +0300

    CI: Two Machines (#402)
    This patch starts using two machines to test testgres.
    
    Key things
    - Option '--init' is used in a command docker run to prevent a problem with lots of zomby processes on altlinux-11, alpine-pg10 and alpine-pg11 targets.
    
    Co-authored-by: Mark G. mark@google.com

--- run_tests.sh
+++ run_tests.sh
@@ -6,6 +6,8 @@ if [ -z ${TEST_FILTER+x} ]; \
 then export TEST_FILTER="TestTestgresLocal or (TestTestgresCommon and (not remote))"; \
 fi
 
+echo NPROC: $(nproc)
+
 # fail early
 echo check that pg_config is in PATH
 command -v pg_config
@@ -25,16 +27,55 @@ rm -f $COVERAGE_FILE
 
 pip install coverage
 
+if [ -n "${TEST_CFG__REMOTE_HOST:-}" ] && [ -n "${TEST_CFG__REMOTE_USERNAME:-}" ]; then
+    cmd_str="ssh"
+
+    if [ -n "${TEST_CFG__REMOTE_PASSWORD:-}" ]; then
+        cmd_str="sshpass -p \"$TEST_CFG__REMOTE_PASSWORD\" $cmd_str"
+    fi
+
+    [ -n "${TEST_CFG__REMOTE_SSH_KEY:-}" ] && cmd_str="$cmd_str -i \"$TEST_CFG__REMOTE_SSH_KEY\""
+    [ -n "${TEST_CFG__REMOTE_PORT:-}" ]    && cmd_str="$cmd_str -p \"$TEST_CFG__REMOTE_PORT\""
+
+    cmd_str="$cmd_str \"$TEST_CFG__REMOTE_USERNAME@$TEST_CFG__REMOTE_HOST\" 'df -T'"
+    REMOTE_FS_STATE_QUERY="$cmd_str"
+else
+    REMOTE_FS_STATE_QUERY=""
+fi
+
+show_fs_state() {
+    df -T
+    if [ -n "$REMOTE_FS_STATE_QUERY" ]; then
+        eval "$REMOTE_FS_STATE_QUERY"
+    fi
+}
+
+# ---------------------------------------- PATH
+
+show_fs_state
+
 # run tests (PATH)
 time coverage run -a -m pytest -l -vvv -n 4 -k "${TEST_FILTER}"
+
+# ---------------------------------------- PG_BIN
+
+show_fs_state
 
 # run tests (PG_BIN)
 PG_BIN=$(pg_config --bindir) \
 time coverage run -a -m pytest -l -vvv -n 4 -k "${TEST_FILTER}"
 
+# ---------------------------------------- PG_CONFIG
+
+show_fs_state
+
 # run tests (PG_CONFIG)
 PG_CONFIG=$(pg_config --bindir)/pg_config \
 time coverage run -a -m pytest -l -vvv -n 4 -k "${TEST_FILTER}"
+
+# ---------------------------------------- pg8000
+
+show_fs_state
 
 # test pg8000
 pip uninstall -y psycopg2
@@ -42,7 +83,12 @@ pip install pg8000
 PG_CONFIG=$(pg_config --bindir)/pg_config \
 time coverage run -a -m pytest -l -vvv -n 4 -k "${TEST_FILTER}"
 
-# show coverage
+# ---------------------------------------- finish
+
+show_fs_state
+
+# ---------------------------------------- coverage
+
 coverage report
 
 pip uninstall -y coverage
