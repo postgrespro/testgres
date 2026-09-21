9ee80f641c220bba1e3bdb4467b3574a1919427e
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-09-22 02:36:54 +0300

    run_tests-darwin: pytest --color=yes (v2)

--- run_tests-darwin.sh
+++ run_tests-darwin.sh
@@ -95,7 +95,7 @@ fs_verification() {
 fs_verification
 
 # run tests (PATH)
-time coverage run -a -m pytest -l -vvv -n auto -k "${TEST_FILTER}"
+time coverage run -a -m pytest -l -vvv -n auto --color=yes -k "${TEST_FILTER}"
 
 # ---------------------------------------- PG_BIN
 
@@ -111,7 +111,7 @@ fs_verification
 
 # run tests (PG_CONFIG)
 PG_CONFIG=$(pg_config --bindir)/pg_config \
-time coverage run -a -m pytest -l -vvv -n auto -k "${TEST_FILTER}"
+time coverage run -a -m pytest -l -vvv -n auto --color=yes -k "${TEST_FILTER}"
 
 # ---------------------------------------- pg8000
 
@@ -121,7 +121,7 @@ fs_verification
 pip uninstall -y psycopg2
 pip install pg8000
 PG_CONFIG=$(pg_config --bindir)/pg_config \
-time coverage run -a -m pytest -l -vvv -n auto -k "${TEST_FILTER}"
+time coverage run -a -m pytest -l -vvv -n auto --color=yes -k "${TEST_FILTER}"
 
 # ---------------------------------------- finish
 
