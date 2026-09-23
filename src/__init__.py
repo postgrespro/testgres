91a9d6529024870c4f402337b474b52ef0a764ee
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-09-23 18:54:45 +0300

    Version 1.16.3

--- src/__init__.py
+++ src/__init__.py
@@ -60,7 +60,7 @@ from testgres.operations.os_ops import OsProcessController
 from testgres.operations.local_ops import LocalOperations
 from testgres.operations.remote_ops import RemoteOperations
 
-__version__ = "1.16.2"
+__version__ = "1.16.3"
 
 __all__ = [
     "get_new_node",
--- tests/test_testgres_common.py
+++ tests/test_testgres_common.py
@@ -121,7 +121,7 @@ class TestTestgresCommon:
         # Author: Mark G.
         assert v.major == 1
         assert v.minor == 16
-        assert v.micro == 2
+        assert v.micro == 3
 
         assert str(v) == testgres_version
         return
